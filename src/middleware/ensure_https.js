bc1097b4630a6701785bd4d23d8a57a19c9fbe86
phaninder pasupula <pasupulaphani@gmail.com>
2017-05-30 13:18:36 +0100

    fix typo

--- src/middleware/ensure_https.js
+++ src/middleware/ensure_https.js
@@ -2,7 +2,7 @@ const config = require('config');
 
 module.exports = function () {
   return function ensureHttps (req, res, next) {
-    if (config.app.ensureHttps && !(req.protocol === "https" || req.headers["x-forwarded-proto"] === "https")) {
+    if (config.general.ensureHttps && !(req.protocol === "https" || req.headers["x-forwarded-proto"] === "https")) {
       const host = req.headers.host;
       if (host !== undefined) {
         res.redirect(`https://${host}${req.url}`);
