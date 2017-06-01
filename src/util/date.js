84027335303b0cbe56446c12fc41d012366b9379
Seweryn Bidolach <sbidolach@op.pl>
2017-06-01 12:58:59 +0100

    Bug fixing

--- src/util/date.js
+++ src/util/date.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 
-exports.dateFormat = function (timestamp) {
+export const dateFormat = timestamp => {
   let date = moment.unix(Math.floor((Number(timestamp/1000))))
   return date.format('DD.MM.YYYY HH:mm:ss')
 }
