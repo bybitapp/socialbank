27e1490ebcf395551df37c20902ff55f88fc7825
Seweryn Bidolach <sbidolach@op.pl>
2017-06-02 10:17:49 +0100

    refactoring and login endpoint

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import { projects } from './projects'
+import { account } from './account'
 
 export default combineReducers({
   projects,
+  account,
   form
 })
