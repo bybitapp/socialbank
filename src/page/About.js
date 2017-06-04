a41d1badaa5ad3ce870f78936189e394f3e9a822
Seweryn Bidolach <sbidolach@op.pl>
2017-06-04 22:56:43 +0100

    style in page about

--- src/page/About.js
+++ src/page/About.js
@@ -18,10 +18,10 @@ class About extends React.Component {
             <div className="page-content">
                 <div className="sb-form-content sb-page" style={styleText}>
                     <div className="mdl-grid">
-                        <div className="mdl-cell mdl-cell--6-col">
-                        <h5>About SocialBank</h5>
-                        <br/><br/>
-                       SocialBank gives donors complete transparency and information regarding finance distribution in charitable and community projects.<br/><br/>
+                        <div className="mdl-cell mdl-cell--12-col">
+                          <h5>About SocialBank</h5>
+                          <br/><br/>
+                          SocialBank gives donors complete transparency and information regarding finance distribution in charitable and community projects.<br/><br/>
                         </div>
                     </div>
                 </div>
