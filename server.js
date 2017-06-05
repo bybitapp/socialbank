71366b7b026447ff4de93c1ba9671fe48121d09a
phaninder pasupula <pasupulaphani@gmail.com>
2017-06-05 16:50:27 +0100

    Refactor accoun route

--- server.js
+++ server.js
@@ -2,25 +2,26 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const passport = require('passport');
-const path = require('path');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
+const path = require('path')
 const cors = require('cors')
+const lusca = require('lusca')
 
-const middleware = require('lib/middleware');
-const routes = require('lib/routes');
-const config = require('./config');
-require('lib/db');
+const middleware = require('lib/middleware')
+const routes = require('lib/routes')
+const config = require('./config')
+require('lib/db')
 
 const server = express()
 
-server.enable("trust proxy");
+server.enable('trust proxy')
 
-server.use(middleware.ensureHttps());
+server.use(middleware.ensureHttps())
 server.use(bodyParser.json())
 server.use(cors())
-server.use(express.static('./build'));
+server.use(express.static('./build'))
 server.use(session({
   resave: true,
   saveUninitialized: true,
@@ -29,15 +30,22 @@ server.use(session({
     url: config.mongoUrl,
     autoReconnect: true
   })
-}));
-server.use(passport.initialize());
-server.use(passport.session());
+}))
+server.use(passport.initialize())
+server.use(passport.session())
+server.use(lusca({
+  csrf: false,
+  xframe: 'SAMEORIGIN',
+  hsts: { maxAge: 31536000 },
+  xssProtection: true,
+  nosniff: true
+}))
 
 server.use(routes)
 
 server.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, './build', 'index.html'))
+})
 
 server.listen(config.app.port, (err) => {
   if (err) throw err
