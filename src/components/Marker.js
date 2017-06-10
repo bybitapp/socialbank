ab7ca9145d9f158bff91856684867bcd0a0f9de2
Seweryn Bidolach <sbidolach@op.pl>
2017-06-10 08:19:17 +0100

    todo tags in code

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -4,6 +4,9 @@ class Marker extends React.Component {
 
   static defaultProps = {};
 
+  // TODO when you click on the marker then user shoudl be redirect to organization
+  // page with information about public projects
+
   render () {
     return (
       <i className="material-icons mdl-list__item-avatar">{this.props.icon}</i>
