92b1f3bb745d4b55ce1e14d7def596a8e6c8cdb8
phaninder pasupula <pasupulaphani@gmail.com>
2017-06-13 14:33:34 +0100

    use this instead of conatant

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 class Select extends React.Component {
+  renderSelectOptions (item) {
+    return (
+      <option key={item.id} value={item.id}>{item.name}</option>
+    )
+  }
+
   render () {
     const { input, name, label, items = [] } = this.props
     return (
       <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
         <select {...input} className="mdl-textfield__input">
-          {items.map(renderSelectOptions)}
+          {items.map(this.renderSelectOptions)}
         </select>
         <label className="mdl-textfield__label" htmlFor={name}>{label}</label>
       </div>
     )
   }
-}
-
-const renderSelectOptions = function (item) {
-  return (
-    <option key={item.id} value={item.id}>{item.name}</option>
-  )
 }
 
 export default Select
