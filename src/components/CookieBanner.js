9473b1464d0f9175462c4e5ed0cce02776502fbb
Yugo Sakamoto <yugoccp@gmail.com>
2017-06-27 08:21:21 -0300

    Cookie Banner at the bottom on mobile

--- src/components/CookieBanner.js
+++ src/components/CookieBanner.js
@@ -13,14 +13,15 @@ class CookieBanner extends React.Component {
   }
 
   render () {
-    const styleBanner = {zIndex: '30', width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.7)', textAlign: 'left'}
+    const styleBanner = {zIndex: '30', width: '100%'}
+    const styleContent = {margin: '6px', backgroundColor: 'rgba(0, 0, 0, 0.7)', textAlign: 'left', borderRadius: '10px'}
     const styleMessage = {margin: '2px', color: '#dadada'}
     const styleClose = {outline: 'none', textDecoration: 'none', color: '#ffffff', fontSize: '16px'}
-    const styleCloseWrapper = {textAlign:'center'}
+    const styleCloseWrapper = {textAlign: 'center'}
     return (
-      <div style={styleBanner}>
+      <div style={styleBanner} className='android-floating-box'>
         {!this.state.acceptedCookie &&
-          <div className='content-grid mdl-grid'>
+          <div style={styleContent} className='content-grid mdl-grid'>
             <div className='mdl-cell mdl-cell--11-col mdl-cell--7-col-tablet mdl-cell--3-col-phone'>
               <p style={styleMessage}>
                 SoTec uses cookies or similar technology to collect information about your access to our website. <a href='/terms'>Learn more</a>
@@ -31,7 +32,7 @@ class CookieBanner extends React.Component {
               <a style={styleClose} onClick={this.acceptCookie}>&times;</a>
             </div>
           </div>
-          }
+        }
       </div>
     )
   }
