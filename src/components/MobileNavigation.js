ce78a1b713fd060085517b535eaeeb2e17d79ed8
Yugo Sakamoto <yugoccp@gmail.com>
2017-06-28 08:46:32 -0300

    Close mobileNavigation on click newsletter

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -11,6 +11,20 @@ const enhance = compose(
 )
 
 class MobileNavigation extends React.Component {
+  componentDidMount () {
+    this.openNewsletterModal = this.openNewsletterModal.bind(this)
+  }
+
+  closeNavigation () {
+    var d = document.querySelector('.mdl-layout')
+    d.MaterialLayout.toggleDrawer()
+  }
+
+  openNewsletterModal () {
+    this.closeNavigation()
+    this.props.setModal('newsletterModal')
+  }
+
   render () {
     const { modal, setModal } = this.props
 
@@ -26,7 +40,7 @@ class MobileNavigation extends React.Component {
               <Link className="mdl-navigation__link" to={'/login'}>Login</Link>
               <Link className="mdl-navigation__link" to={'/registration'}>Register</Link>
               <Link className="mdl-navigation__link" to={'/contact'}>Contact</Link>
-              <a className="mdl-navigation__link" onClick={() => setModal('newsletterModal')}>
+              <a className="mdl-navigation__link" onClick={() => this.openNewsletterModal()}>
                 Subscribe for updates
               </a>
             </div>
@@ -35,7 +49,7 @@ class MobileNavigation extends React.Component {
               <Link className="mdl-navigation__link" to={'/contact'}>Contact</Link>
               <Link className="mdl-navigation__link" to={'/me'}>Account</Link>
               <Link className="mdl-navigation__link" to={'/logout'}>Logout</Link>
-              <a className="mdl-navigation__link" onClick={() => setModal('newsletterModal')}>
+              <a className="mdl-navigation__link" onClick={() => this.openNewsletterModal()}>
                 Subscribe for updates
               </a>
             </div>
