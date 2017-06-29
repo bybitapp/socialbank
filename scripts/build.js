6c686c36ad8ce708aa722e1a444060d04cf5a017
phaninder pasupula <pasupulaphani@gmail.com>
2017-06-29 19:43:25 +0100

    Debug webpack

--- scripts/build.js
+++ scripts/build.js
@@ -100,7 +100,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
   )
 
 // Create the production build and print the deployment instructions.
-function build(previousFileSizes) {
+function build (previousFileSizes) {
   console.log('Creating an optimized production build...')
 
   let compiler = webpack(config)
