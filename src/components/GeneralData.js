68659e22692153050752673c1955a9fd5026e985
Yugo Sakamoto <yugoccp@gmail.com>
2017-07-08 11:51:26 -0300

    Add update user name and phone

--- src/components/GeneralData.js
+++ src/components/GeneralData.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
-import { reduxForm, Field, change } from 'redux-form'
+import { reduxForm, Field, change, SubmissionError } from 'redux-form'
+import {toastr} from 'react-redux-toastr'
 import Input from './Input'
 import Auth from '../modules/Auth'
+import { updateAccount } from '../actions'
+
+const validate = values => {
+  const errors = {}
+
+  if (!values.phone) {
+    errors.phone = 'Required'
+  }
+  if (!values.name) {
+    errors.name = 'Required'
+  } else if (values.name.length < 3) {
+    errors.name = 'Must be 3 characters or more'
+  } else if (values.name.length > 255) {
+    errors.name = 'Must be 255 characters or less'
+  }
+
+  return errors
+}
 
 function mapStateToProps (state) {
   const { account } = state
@@ -15,11 +34,25 @@ function mapStateToProps (state) {
 const enhance = compose(
   connect(mapStateToProps),
   reduxForm({
-    form: 'generalInformation'
+    form: 'generalInformation',
+    validate,
+    onSubmit: (values, dispatch, ownProps) => {
+      return new Promise((resolve, reject) => {
+        dispatch(updateAccount(values, (_error) => {
+          if (!_error) {
+            toastr.success('Updated!', 'Your account has been updated')
+            resolve()
+          } else {
+            reject(new SubmissionError({_error}))
+          }
+        }))
+      })
+    }
   })
 )
 
 const updateData = (account, dispatch) => {
+  dispatch(change('generalInformation', 'name', account.profile.name))
   dispatch(change('generalInformation', 'email', account.email))
   dispatch(change('generalInformation', 'phone', account.phone))
 }
@@ -41,17 +74,24 @@ class GeneralInformation extends React.Component {
   }
 
   render () {
+    const { handleSubmit } = this.props
     return (
       <div>
         <h5>General Information</h5>
-        <div className='mdl-grid'>
-          <div className='mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet'>
-            <Field name='email' label='Email' component={Input} disabled />
+        <form onSubmit={handleSubmit} >
+          <div className='mdl-grid'>
+            <div className='mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet'>
+              <Field name='name' label='Name' component={Input} />
+              <Field name='email' label='Email' component={Input} disabled />
+            </div>
+            <div className='mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet'>
+              <Field name='phone' label='Phone Number' component={Input} />
+              <div className='sb-details-button'>
+                <button className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored' type='submit'>Save</button>
+              </div>
+            </div>
           </div>
-          <div className='mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet'>
-            <Field name='phone' label='Phone Number' component={Input} disabled />
-          </div>
-        </div>
+        </form>
       </div>)
   }
 }
--- src/reducers/account.js
+++ src/reducers/account.js
@@ -1,9 +1,11 @@
-import { REGISTER_ACCOUNT_SUCCESS, LOGIN_SUCCESS, LOGOUT_SUCCESS } from '../constants/ActionTypes'
+import { REGISTER_ACCOUNT_SUCCESS, UPDATE_ACCOUNT_SUCCESS, LOGIN_SUCCESS, LOGOUT_SUCCESS } from '../constants/ActionTypes'
 
 export const account = (state = [], action) => {
   switch (action.type) {
     case REGISTER_ACCOUNT_SUCCESS:
       return (action.data) ? action.data : state
+    case UPDATE_ACCOUNT_SUCCESS:
+      return Object.assign({}, state, action.data)
     case LOGIN_SUCCESS:
       return (action.data) ? action.data : state
     case LOGOUT_SUCCESS:
