d6ecb58da55cff5e4a25977aed8c0503666cac5f
Seweryn Bidolach <sbidolach@op.pl>
2017-07-15 13:36:43 +0100

    new layout

--- src/page/Home.js
+++ src/page/Home.js
@@ -82,19 +82,115 @@ class Home extends React.Component {
           <div className='content-wrap'>
             <div className='container clearfix'>
               <div className='col_one_third nobottommargin center'>
-                <a href='#'><img src='images/appshowcase/s1.png' alt='Image' className='bottommargin-sm' /></a>
+                <a href='#'><img src='images/new/s1.png' alt='Image' className='bottommargin-sm' /></a>
                 <h4>Responsive Layout</h4>
                 <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
               </div>
               <div className='col_one_third nobottommargin center'>
-                <a href='#'><img src='images/appshowcase/s4.png' alt='Image' className='bottommargin-sm' /></a>
+                <a href='#'><img src='images/new/s4.png' alt='Image' className='bottommargin-sm' /></a>
                 <h4>Retina Ready Graphics</h4>
                 <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
               </div>
               <div className='col_one_third nobottommargin center col_last'>
-                <a href='#'><img src='images/appshowcase/s5.png' alt='Image' className='bottommargin-sm' /></a>
+                <a href='#'><img src='images/new/s5.png' alt='Image' className='bottommargin-sm' /></a>
                 <h4>Powerful Performance</h4>
                 <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
+              </div>
+            </div>
+            <div className='section nobottommargin' style={{paddingBottom: '150px'}}>
+              <div className='hidden-sm hidden-xs' style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', background: 'transparent url("images/new/ipad-section.png") bottom right no-repeat' }} />
+              <div className='container clearfix' style={{ zIndex: 1 }}>
+                <div className='col-md-6 nobottommargin'>
+                  <div className='heading-block topmargin-sm'>
+                    <h2>Awesome Scalable Apps</h2>
+                    <span>Our Template acts &amp; behaves truly as a Canvas.</span>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem maiores pariatur voluptatem placeat laborum iste accusamus nam unde, iure id.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet cumque, perferendis accusamus porro illo exercitationem molestias, inventore obcaecati ut omnis voluptatibus ratione odio amet magnam quidem tempore necessitatibus quaerat, voluptates excepturi voluptatem, veritatis qui temporibus.</p>
+                  <a href='#' className='button button-border button-rounded button-large button-dark noleftmargin'>Start Trial</a>
+                </div>
+              </div>
+            </div>
+            <div className='section notopmargin nobottommargin'>
+              <div className='container clearfix'>
+                <div className='col_half nobottommargin topmargin-lg'>
+                  <img src='images/new/iphone-solid.png' alt='Image' className='center-block' />
+                </div>
+                <div className='col_half nobottommargin topmargin-lg col_last'>
+                  <div className='heading-block topmargin-lg'>
+                    <h2>Typically Responsive</h2>
+                    <span>Our App scales beautifully to different Devices.</span>
+                  </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet cumque, perferendis accusamus porro illo exercitationem molestias, inventore obcaecati ut omnis voluptatibus ratione odio amet magnam quidem tempore necessitatibus quaerat, voluptates excepturi voluptatem, veritatis qui temporibus.</p>
+                  <a href='#' className='button button-border button-rounded button-large button-dark noleftmargin'>View Gallery</a>
+                </div>
+              </div>
+            </div>
+            <div className='section dark notopmargin' style={{ paddingTop: '60px' }}>
+              <div className='container clearfix'>
+                <div className='col_one_third'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-screen' /></a>
+                    </div>
+                    <h3>Responsive Layout</h3>
+                    <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
+                  </div>
+                </div>
+                <div className='col_one_third'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-eye' /></a>
+                    </div>
+                    <h3>Retina Ready Graphics</h3>
+                    <p>Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Retina Icons, Fonts &amp; all others graphics are optimized.</p>
+                  </div>
+                </div>
+                <div className='col_one_third col_last'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-beaker' /></a>
+                    </div>
+                    <h3>Powerful Performance</h3>
+                    <p>Canvas includes tons of optimized code that are completely customizable and deliver unmatched fast performance.</p>
+                  </div>
+                </div>
+                <div className='clear' />
+                <div className='col_one_third nobottommargin'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-ok' /></a>
+                    </div>
+                    <h3>12+ Header Designs</h3>
+                    <p>We have included 12+ Header + Menu Designs for your convenience so that you can match your style.</p>
+                  </div>
+                </div>
+                <div className='col_one_third nobottommargin'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-thumbs-up' /></a>
+                    </div>
+                    <h3>Awesome Mega menu</h3>
+                    <p>Completely Customizable 2 Columns, 3 Columns, 4 Columns &amp; 5 Columns Mega Menus are available with Canvas.</p>
+                  </div>
+                </div>
+                <div className='col_one_third nobottommargin col_last'>
+                  <div className='feature-box fbox-plain'>
+                    <div className='fbox-icon'>
+                      <a href='#'><i className='icon-magnet' /></a>
+                    </div>
+                    <h3>Attractive Sticky Menu</h3>
+                    <p>Smooth &amp; Stylish Sticky Menu is what will make your Website differentiate with others.</p>
+                  </div>
+                </div>
+                <div className='clear' /><div className='line' />
+                <div className='heading-block center'>
+                  <h2>Looks beautiful on all displays.</h2>
+                </div>
+                <div style={{ position: 'relative', marginBottom: '-60px' }} data-height-lg='415' data-height-md='342' data-height-sm='262' data-height-xs='160' data-height-xxs='102'>
+                  <img src='images/new/chrome.png' style={{position: 'absolute', top: 0, left: 0}} data-animate='fadeInUp' alt='Chrome' />
+                  <img src='images/new/ipad3.png' style={{position: 'absolute', top: 0, left: 0}} data-animate='fadeInUp' data-delay='300' alt='iPad' />
+                </div>
               </div>
             </div>
             <div className='container clearfix'>
