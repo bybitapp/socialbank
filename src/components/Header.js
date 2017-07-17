d6b596cc0396bc45b92ae67e45c4a430e03cc9f2
Seweryn Bidolach <sbidolach@op.pl>
2017-07-17 14:09:46 +0100

    page in new layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,21 +32,21 @@ class Header extends React.Component {
     return (
       <header id='header' className='full-header'>
         <div id='header-wrap'>
-          <div className='container clearfix'>
+          <div className='container clearfix' style={{ padding: 0 }}>
             <div id='primary-menu-trigger'><i className='icon-reorder' /></div>
-            <div id='logo'>
-              <a href='/' className='standard-logo' data-dark-logo='images/logo-dark.png'><img src='images/logo.png' alt='Canvas Logo' /></a>
-              <a href='/' className='retina-logo' data-dark-logo='images/logo-dark@2x.png'><img src='images/logo@2x.png' alt='Canvas Logo' /></a>
+            <div id='logo' style={{ borderRight: 0 }}>
+              <a href='/' className='standard-logo' data-dark-logo='images/logo-dark.png'><img src='images/logo.png' alt='sotec logo' /></a>
+              <a href='/' className='retina-logo' data-dark-logo='images/logo-dark@2x.png'><img src='images/logo@2x.png' alt='sotec logo' /></a>
             </div>
             <nav id='primary-menu' >
               {Auth.isUserAuthenticated() ? (
-                <ul>
+                <ul style={{ borderRight: 0 }}>
                   <li><Link to={'/me'}>Account</Link></li>
                   <li><Link to={'/contact'}>Contact</Link></li>
                   <li><Link to={'/logout'}>Logout</Link></li>
                 </ul>
               ) : (
-                <ul>
+                <ul style={{ borderRight: 0 }}>
                   <li><Link to={'/contact'}>Contact</Link></li>
                   <li><a href='/login'>Login</a></li>
                   <li><Link to={'/register'}>Register</Link></li>
