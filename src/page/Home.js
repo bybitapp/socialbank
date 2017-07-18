e72e455641ae505ea5d0c701b391c94b7467f5eb
Seweryn Bidolach <sbidolach@op.pl>
2017-07-18 14:17:04 +0100

    Update Home.js

--- src/page/Home.js
+++ src/page/Home.js
@@ -24,7 +24,7 @@ const TopView = () => (
         <div className='vertical-middle no-fade'>
           <div className='col-md-6 fright nobottommargin' data-animate='fadeIn'>
             <div className='emphasis-title'>
-              <h1 data-style-lg='font-size: 52px;' data-style-md='font-size: 44px;'>Revolutionize the way every <strong style={{ color: '#00D0A7' }}>charity</strong> manages its expenses.</h1>
+              <h1 data-style-lg='font-size: 52px;' data-style-md='font-size: 44px;'>Revolutionize the way your <strong style={{ color: '#00D0A7' }}>charity</strong> manages its expenses.</h1>
             </div>
             <div>
               <Link to={'/register'} className='button button-desc button-border button-rounded nomargin'>
