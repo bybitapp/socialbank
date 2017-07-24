f66bbfa664fffc1c5a393685d7cc71719b0dd084
Yugo Sakamoto <yugoccp@gmail.com>
2017-07-24 00:03:25 -0300

    [WIP] Styling

--- src/components/CardDetailForm.js
+++ src/components/CardDetailForm.js
@@ -6,6 +6,19 @@ const customStyles = {
   overlay: {zIndex: 4}
 }
 
+const preloaderStyle = {margin: 'auto', textAlign: 'center', display: 'block'}
+const loaderStyle = {widht: '28px', height: '28px'}
+
+const formatCardNnumber = (cardNumber) => {
+  const cardNumberArray = cardNumber.match(/.{1,4}/g)
+  console.log(cardNumberArray)
+  let formattedCardNumber = ''
+  cardNumberArray.forEach((num) => {
+    formattedCardNumber += num + '\t'
+  })
+  return formattedCardNumber
+}
+
 class CardDetailForm extends React.Component {
   onClose () {
     const { handleClose } = this.props
@@ -36,14 +49,16 @@ class CardDetailForm extends React.Component {
                 { cardDetail
                   ? <div>
                     <div className='sb-card-name'>{ cardDetail.cardName }</div>
-                    <div className='sb-card-number'>{ cardDetail.cardNumber }</div>
+                    <div className='sb-card-number'>{ formatCardNnumber(cardDetail.cardNumber) }</div>
                     <div className='sb-card-cvv'>{ cardDetail.cvv }</div>
                     <div className='sb-card-startdate'>CREATED AT { cardDetail.startDate }</div>
                     <div className='sb-card-enddate'>EXPIRES { cardDetail.endDate }</div>
                     <div className='sb-card-brand'><img src={`images/credit_card/brands/${cardDetail.cardBrand}.png`} alt={cardDetail.cardBrand} /></div>
                   </div>
-                  : <div>
-                    <span className='dark preloader' />
+                  : <div style={preloaderStyle}>
+                    <div >
+                      <span className='dark preloader' style={loaderStyle} />
+                    </div>
                     <span>Loading...</span>
                   </div>
                 }
