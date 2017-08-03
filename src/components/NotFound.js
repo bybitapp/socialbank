b70b3fb3edc1ba4e0b0807e41656a0e9696667bf
phaninder pasupula <pasupulaphani@gmail.com>
2017-08-03 17:59:44 +0100

    Refactor not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,9 +5,16 @@ export default class NotFound extends Component {
     const { className, ...props } = this.props
     return (
       <div className='NotFound' {...props}>
-        <h1>
-          404 <small>Not Found :(</small>
-        </h1>
+        <p>
+          <h1>
+            This page isn&#39;t available
+            <br />
+            <h2><small>The link you followed may be broken.</small></h2>
+          </h1>
+        </p>
+        <p>
+          <h2><a href='http://sotec.io'>Go to <u>SoTec.io</u></a></h2>
+        </p>
       </div>
     )
   }
