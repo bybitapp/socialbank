c22cd70e736f41695e1334dad197d0c6fc96602a
Yugo Sakamoto <yugoccp@gmail.com>
2017-08-04 23:02:14 -0300

    Add policy pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,15 @@ class Footer extends React.Component {
   render () {
     return (
       <footer id='footer' className='dark'>
-        <div id='copyrights' style={{ fontSize: '16px' }}>
+        <div id='copyrights' style={{ fontSize: '14px' }}>
           <div className='container clearfix'>
+            <div id='disclaimer' style={{ textAlign: 'center', fontSize: '12px', marginBottom: '40px' }}>
+              Ixaris Systems Ltd is authorised and regulated by the Financial Conduct Authority under the Payment Service Regulations 2009 for the provision of payment services. Registration number 540990. Registered address 10 Midford Place, London, W1T 5AE, United Kingdom. Ixaris Visa cards are issued by IDT Financial Services Ltd., pursuant to a licence from Visa Europe. Ixaris Mastercard cards are issued by IDT Financial Services Ltd., pursuant to licence by Mastercard International. Mastercard is a registered trademark of Mastercard International.
+            </div>
             <div className='col_half'>
               Copyrights &copy; 2017 All Rights Reserved by SoTec.<br />
               <div className='copyright-links'>
-                <a href='/terms'>Terms of Use</a> / <a href='/privacy'>Privacy Policy</a>
+                <a href='/terms'>Terms of Use</a> / <a href='/complaints'>Complaints Policy</a> / <a href='/privacy'>Privacy Policy</a>
               </div>
             </div>
             <div className='col_half col_last tright'>
@@ -29,18 +32,6 @@ class Footer extends React.Component {
               <i className='icon-headphones' /> +44 7437 893 938
             </div>
           </div>
-        </div>
-        <div id='disclaimer' className='container clearfix'>
-          <br />
-          Ixaris Systems Ltd is authorised and regulated by the Financial Conduct Authority under the Payment Service Regulations 2009 for the provision of payment services. Registration number 540990. Registered address 10 Midford Place, London, W1T 5AE, United Kingdom.
-          <br />
-          <br />
-          Ixaris Visa cards are issued by IDT Financial Services Ltd., pursuant to a licence from Visa Europe.
-          <br />
-          <br />
-          Ixaris Mastercard cards are issued by IDT Financial Services Ltd., pursuant to licence by Mastercard International. Mastercard is a registered trademark of Mastercard International.
-          <br />
-          <br />
         </div>
       </footer>
     )
