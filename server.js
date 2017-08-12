247936553c314122602624c6d2b30aa84a526c1b
phaninder pasupula <pasupulaphani@gmail.com>
2017-08-12 21:58:00 +0100

    Limit the payload size

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ process.on('unhandledRejection', error => {
 server.enable('trust proxy')
 
 server.use(middleware.ensureHttps())
-server.use(bodyParser.json())
+server.use(bodyParser.json({limit: '512kb'}))
 server.use(cors())
 server.use(express.static('./build'))
 server.use(session({
