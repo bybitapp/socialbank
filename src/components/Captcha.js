544f7889941a5981d1931f780734545dfc501586
phaninder pasupula <pasupulaphani@gmail.com>
2017-08-14 07:56:15 +0100

    Fix captcha

--- src/components/Captcha.js
+++ src/components/Captcha.js
@@ -1,6 +1,32 @@
 import React from 'react'
 
-class Captcha extends React.Component {
+const isReady = () => typeof window !== 'undefined' && typeof window.grecaptcha !== 'undefined'
+
+let readyCheck
+
+export default class Captcha extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      ready: isReady(),
+      widget: null
+    }
+
+    if (!this.state.ready) {
+      readyCheck = setInterval(this._updateReadyState.bind(this), 1000)
+    }
+  }
+
+  _updateReadyState () {
+    if (isReady()) {
+      this.setState({
+        ready: true
+      })
+
+      clearInterval(readyCheck)
+    }
+  }
+
   render () {
     return <div ref={div => (this.container = div)} />
   }
@@ -8,13 +34,15 @@ class Captcha extends React.Component {
   componentDidMount () {
     const { input: { onChange }, sitekey } = this.props
 
-    if (window.grecaptcha) {
+    if (this.state.ready) {
       window.grecaptcha.render(this.container, {
         sitekey,
         callback: onChange
       })
     }
   }
-}
 
-export default Captcha
+  componentWillUnmount () {
+    clearInterval(readyCheck)
+  }
+}
