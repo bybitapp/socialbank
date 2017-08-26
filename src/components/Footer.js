3ca7e3237bf371bee5c1b91922b0c2e511234178
Yugo Sakamoto <yugoccp@gmail.com>
2017-08-26 10:18:56 -0300

    Styling footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,34 +9,19 @@ const Disclaimer = () => (
 const Copyright = () => (
   <div>
     Copyrights &copy; 2017 All Rights Reserved by SoTec.<br />
-    <div className='copyright-links'>
-      <a href='/terms'>Terms of Use</a> / <a href='/complaints'>Complaints Policy</a> / <a href='/privacy'>Privacy Policy</a>
-    </div>
   </div>
 )
 
-const Contact = () => (
-  <div>
-    <div className='fright clearfix'>
-      <a href='https://www.facebook.com/SoTecUK' target='_blank' rel='noopener noreferrer' className='social-icon si-small si-borderless si-facebook'>
-        <i className='icon-facebook' />
-        <i className='icon-facebook' />
-      </a>
-      <a href='https://twitter.com/SoTec_UK' target='_blank' rel='noopener noreferrer' className='social-icon si-small si-borderless si-twitter'>
-        <i className='icon-twitter' />
-        <i className='icon-twitter' />
-      </a>
-    </div>
-    <div className='clear' />
-    <i className='icon-envelope2' /> contact@sotec.io
-    <span className='middot'>&middot;</span>
-    <i className='icon-headphones' /> +44 7437 893 938
+const CopyrightLinks = () => (
+  <div className='copyright-links'>
+    <a href='/terms'>Terms of Use</a> / <a href='/complaints'>Complaints Policy</a> / <a href='/privacy'>Privacy Policy</a>
   </div>
 )
 
 const ProductLinks = () => (
   <div>
-    <ul className='list-inline links'><p className='footertitle'>PRODUCT</p>
+    <ul className='list-unstyled sb-links'>
+      <p>PRODUCT</p>
       <li><a href='https://www.sotec.io/faq'>FAQ</a></li>
       <li><a href='https://www.sotec.io/pricing'>Pricing</a></li>
     </ul>
@@ -45,7 +30,8 @@ const ProductLinks = () => (
 
 const CompanyLinks = () => (
   <div>
-    <ul className='list-inline links'><p className='footertitle'>COMPANY</p>
+    <ul className='list-unstyled sb-links'>
+      <p>COMPANY</p>
       <li><a href='https://www.sotec.io/about'>About us</a></li>
       <li><a href='https://medium.com/sotec'>Blog</a></li>
     </ul>
@@ -54,12 +40,13 @@ const CompanyLinks = () => (
 
 const ConstactLinks = () => (
   <div>
-    <ul className='list-inline links'><p className='footertitle'>CONTACT</p>
+    <ul className='list-unstyled sb-links'>
+      <p>CONTACT</p>
       <li><a href='https://www.sotec.io/contact'>Contact us</a></li>
       <li><i className='icon-envelope2' /> contact@sotec.io</li>
       <li><i className='icon-headphones' /> +44 7437 893 938</li>
       <li>
-        <div className='fright clearfix'>
+        <div className='clearfix'>
           <a href='https://www.facebook.com/SoTecUK' target='_blank' rel='noopener noreferrer' className='social-icon si-small si-borderless si-facebook'>
             <i className='icon-facebook' />
             <i className='icon-facebook' />
@@ -75,37 +62,40 @@ const ConstactLinks = () => (
 )
 
 const FooterLogo = () => (
-  <img src='images/logo-dark.png' alt='logo' style={{width: '90px'}} />
+  <img src='images/logo-dark_nomargin.png' alt='logo' style={{width: '90px'}} />
 )
 
 class Footer extends React.Component {
   render () {
     return (
       <footer id='footer' className='dark'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-4 col-md-offset-1 col-sm-6 col-xs-6'>
-              <FooterLogo />
-            </div>
-            <div className='col-md-2 col-sm-6 col-xs-6'>
-              <ProductLinks />
-            </div>
-            <div className='col-md-2 col-sm-6 col-xs-6'>
-              <CompanyLinks />
-            </div>
-            <div className='col-md-2 col-sm-6 col-xs-6'>
-              <ConstactLinks />
-            </div>
-          </div>
-        </div>
         <div id='copyrights' style={{ fontSize: '14px' }}>
           <div className='container clearfix'>
-            <Disclaimer />
-            <div className='col_half'>
-              <Copyright />
+            <div className='row'>
+              <div className='col-md-5 col-md-offset-1 col-sm-6 col-xs-6'>
+                <FooterLogo />
+              </div>
+              <div className='col-md-2 col-sm-6 col-xs-6'>
+                <ProductLinks />
+              </div>
+              <div className='col-md-2 col-sm-6 col-xs-6'>
+                <CompanyLinks />
+              </div>
+              <div className='col-md-2 col-sm-6 col-xs-6'>
+                <ConstactLinks />
+              </div>
             </div>
-            <div className='col_half col_last tright'>
-              <Contact />
+            <div style={{borderTop: '1px solid #999'}} />
+            <div className='row topmargin-sm'>
+              <Disclaimer />
+            </div>
+            <div className='row bottommargin'>
+              <div className='col_half'>
+                <Copyright />
+              </div>
+              <div className='col_half col_last tright'>
+                <CopyrightLinks />
+              </div>
             </div>
           </div>
         </div>
