b8db1868a1d1361c237879ef9179eb4cf51b9d4d
Yugo Sakamoto <yugoccp@gmail.com>
2017-09-03 23:20:48 -0300

    [WIP] fix faq

--- src/page/Faq.js
+++ src/page/Faq.js
@@ -3,8 +3,61 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+const FaqItem = ({question, answer}) => {
+  let showAnswer = false
+
+  const toggleAnswer = () => {
+    console.log(showAnswer)
+    showAnswer = !showAnswer
+  }
+
+  toggleAnswer.bind(this)
+
+  console.log('FaqItem')
+  console.log(showAnswer)
+
+  return (<div className='toggle faq'>
+    <div className='togglet' onClick={toggleAnswer}>{question}</div>
+    <div style={{display: this.showAnswer ? 'block' : 'none'}}>{answer}</div>
+  </div>)
+}
+
+const BasicFaq = ({question, answer, filterValue}) => {
+  console.log(filterValue)
+  if (filterValue === '*' || filterValue === 'faq-basic') {
+    return <FaqItem question={question} answer={answer} />
+  }
+  return null
+}
+
+const SecurityFaq = ({question, answer, filterValue}) => {
+  if (filterValue === '*' || filterValue === 'faq-security') {
+    return <FaqItem question={question} answer={answer} />
+  }
+  return null
+}
+
+const UseFaq = ({question, answer, filterValue}) => {
+  if (filterValue === '*' || filterValue === 'faq-use') {
+    return <FaqItem question={question} answer={answer} />
+  }
+  return null
+}
+
 class Faq extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {filterValue: '*'}
+    this.onFilter = this.onFilter.bind(this)
+  }
+
+  onFilter (filter) {
+    this.setState({filterValue: filter})
+  }
+
   render () {
+    const {filterValue} = this.state
+    console.log(filterValue)
     return (
       <div id='wrapper' className='clearfix'>
         <Header />
@@ -23,68 +76,68 @@ class Faq extends React.Component {
             <div className='container clearfix'>
               <div className='postcontent nobottommargin clearfix'>
                 <ul id='portfolio-filter' className='portfolio-filter clearfix'>
-                  <li className=''><a data-filter='*' className='btn'>All</a></li>
-                  <li className=''><a data-filter='.faq-basic' className='btn'>Basic questions</a></li>
-                  <li className=''><a data-filter='.faq-using-sotec' className='btn'>How to use SoTec</a></li>
-                  <li className=''><a data-filter='.faq-security' className='btn'>Security questions</a></li>
+                  <li className=''><a onClick={() => this.onFilter('*')} className='btn'>All</a></li>
+                  <li className=''><a onClick={() => this.onFilter('faq-basic')} className='btn'>Basic questions</a></li>
+                  <li className=''><a onClick={() => this.onFilter('faq-use')} className='btn'>How to use SoTec</a></li>
+                  <li className=''><a onClick={() => this.onFilter('faq-security')} className='btn'>Security questions</a></li>
                 </ul>
                 <div className='clear' />
                 <div id='faqs' className='faqs'>
 
-                  <div className='toggle faq faq-basic'>
-                    <div className='togglet'>What is SoTec?</div>
-                    <div className='togglec' style={{display: 'none'}}>SoTec provides payment card solutions to manage and simplify organisation’s funding and expenses. Our solution provides increased internal transparency through micropayments and automated expense reporting by utilizing virtual and physical prepaid debit cards on top of the award winning, low cost, open payment cloud platform.</div>
-                  </div>
+                  <BasicFaq
+                    filterValue={filterValue}
+                    question='What is SoTec?'
+                    answer='SoTec provides payment card solutions to manage and simplify organisation’s funding and expenses. Our solution provides increased internal transparency through micropayments and automated expense reporting by utilizing virtual and physical prepaid debit cards on top of the award winning, low cost, open payment cloud platform.' />
 
-                  <div className='toggle faq faq-basic'>
-                    <div className='togglet'>Who is the issuer of the SoTec cards?</div>
-                    <div className='togglec' style={{display: 'none'}}>SoTec cards are issued by one of Europe’s largest issuers of Prepaid Visa(s), IDT Financial Services Limited pursuant to a license from Visa International Incorporated. IDT Financial Services Limited is a regulated bank, licensed by the Financial Services Commission, Gibraltar. Registered Office: 57-63 Line Wall Road, Gibraltar. Registered No. 95716.</div>
-                  </div>
+                  <BasicFaq
+                    filterValue={filterValue}
+                    question='Who is the issuer of the SoTec cards?'
+                    answer='SoTec cards are issued by one of Europe’s largest issuers of Prepaid Visa(s), IDT Financial Services Limited pursuant to a license from Visa International Incorporated. IDT Financial Services Limited is a regulated bank, licensed by the Financial Services Commission, Gibraltar. Registered Office: 57-63 Line Wall Road, Gibraltar. Registered No. 95716.' />
 
-                  <div className='toggle faq faq-basic'>
-                    <div className='togglet'>Is SoTec for us?</div>
-                    <div className='togglec' style={{display: 'none'}}>SoTec was created for dynamic businesses with decentralized spending cultures, including the sharing of credit cards, collections of receipts, expense reports and reimbursements, causing confusion and complexity for everyone in the workplace.</div>
-                  </div>
+                  <BasicFaq
+                    filterValue={filterValue}
+                    question='Is SoTec for us?'
+                    answer='SoTec was created for dynamic businesses with decentralized spending cultures, including the sharing of credit cards, collections of receipts, expense reports and reimbursements, causing confusion and complexity for everyone in the workplace.' />
 
-                  <div className='toggle faq faq-basic'>
-                    <div className='togglet'>Is SoTec a credit card?</div>
-                    <div className='togglec' style={{display: 'none'}}>No, SoTec initially only offers Prepaid Visa cards, where only the money loaded to your account is available to spend.</div>
-                  </div>
+                  <BasicFaq
+                    filterValue={filterValue}
+                    question='Is SoTec a credit card?'
+                    answer='No, SoTec initially only offers Prepaid Visa cards, where only the money loaded to your account is available to spend.' />
 
-                  <div className='toggle faq faq-basic'>
-                    <div className='togglet'>How do SoTec cards work?</div>
-                    <div className='togglec' style={{display: 'none'}}>Prepaid Visa cards are accepted by more than 30 million merchants worldwide. The virtual SoTec cards can be used to make any work related online purchases. Prepaid cards can be used to purchase goods in the same way as a debit or credit card but you can only make transactions when there is money in your account.</div>
-                  </div>
+                  <BasicFaq
+                    filterValue={filterValue}
+                    question='How do SoTec cards work?'
+                    answer='Prepaid Visa cards are accepted by more than 30 million merchants worldwide. The virtual SoTec cards can be used to make any work related online purchases. Prepaid cards can be used to purchase goods in the same way as a debit or credit card but you can only make transactions when there is money in your account.' />
 
-                  <div className='toggle faq faq-using-sotec'>
-                    <div className='togglet'>In which countries is SoTec available?</div>
-                    <div className='togglec' style={{display: 'none'}}>SoTec is initially available to companies in the United Kingdom. More countries will be added in due course so watch this space.</div>
-                  </div>
+                  <UseFaq
+                    filterValue={filterValue}
+                    question='In which countries is SoTec available?'
+                    answer='SoTec is initially available to companies in the United Kingdom. More countries will be added in due course so watch this space.' />
 
-                  <div className='toggle faq faq-using-sotec'>
-                    <div className='togglet'>How can our company transfer funds to the SoTec cards?</div>
-                    <div className='togglec' style={{display: 'none'}}>The virtual SoTec cards are Prepaid Visa cards. To use the cards, your company needs to have money in the SoTec account. An easy way of providing funds to SoTec is by use of a bank wire. After a few business days your funds will be received into a ring fenced account (which means that neither we or anyone else can access your money).</div>
-                  </div>
+                  <UseFaq
+                    filterValue={filterValue}
+                    question='How can our company transfer funds to the SoTec cards?'
+                    answer='The virtual SoTec cards are Prepaid Visa cards. To use the cards, your company needs to have money in the SoTec account. An easy way of providing funds to SoTec is by use of a bank wire. After a few business days your funds will be received into a ring fenced account (which means that neither we or anyone else can access your money).' />
 
-                  <div className='toggle faq faq-using-sotec'>
-                    <div className='togglet'>Can SoTec cards be used everywhere?</div>
-                    <div className='togglec' style={{display: 'none'}}>SoTec cards can be used in more than 30 million locations worldwide, online and in stores, making corporate purchasing easy and convenient – just look for the Visa Acceptance Mark.</div>
-                  </div>
+                  <UseFaq
+                    filterValue={filterValue}
+                    question='Can SoTec cards be used everywhere?'
+                    answer='SoTec cards can be used in more than 30 million locations worldwide, online and in stores, making corporate purchasing easy and convenient – just look for the Visa Acceptance Mark.' />
 
-                  <div className='toggle faq faq-using-sotec'>
-                    <div className='togglet'>Do I still need to do my expense reports when using SoTec?</div>
-                    <div className='togglec' style={{display: 'none'}}>No, the founding idea behind SoTec is to get rid of the expense reports. As a Manager, you can monitor all transactions in real time. All transactions can be directly exported to most accounting systems.</div>
-                  </div>
+                  <UseFaq
+                    filterValue={filterValue}
+                    question='Do I still need to do my expense reports when using SoTec?'
+                    answer='No, the founding idea behind SoTec is to get rid of the expense reports. As a Manager, you can monitor all transactions in real time. All transactions can be directly exported to most accounting systems.' />
 
-                  <div className='toggle faq faq-security'>
-                    <div className='togglet'>What is SoTec’s fraud policy?</div>
-                    <div className='togglec' style={{display: 'none'}}>With SoTec, all purchases are tracked in real-time, so if you notice something suspicious, you can block the cards immediately from the SoTec web app. You are also covered by Visa Zero Liability Policy - for more information, visit <a href='https://www.visa.com/chip/personal/security/zero-liability.jsp' target='_blank' rel='noopener noreferrer'>Visa</a>.</div>
-                  </div>
+                  <SecurityFaq
+                    filterValue={filterValue}
+                    question='What is SoTec’s fraud policy?'
+                    answer='With SoTec, all purchases are tracked in real-time, so if you notice something suspicious, you can block the cards immediately from the SoTec web app. You are also covered by Visa Zero Liability Policy - for more information, visit <a href="https://www.visa.com/chip/personal/security/zero-liability.jsp" target="_blank" rel="noopener noreferrer">Visa</a>.' />
 
-                  <div className='toggle faq faq-security'>
-                    <div className='togglet'>How will our data be protected?</div>
-                    <div className='togglec' style={{display: 'none'}}>We take security very seriously. All data passed between our servers and third parties is 2048-bit SSL encrypted.</div>
-                  </div>
+                  <SecurityFaq
+                    filterValue={filterValue}
+                    question='How will our data be protected?'
+                    answer='We take security very seriously. All data passed between our servers and third parties is 2048-bit SSL encrypted.' />
 
                 </div>
               </div>
