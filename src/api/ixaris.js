c9c3d99dea851593269f34acca5327c87a3d51aa
phaninder pasupula <pasupulaphani@gmail.com>
2017-10-01 19:08:56 +0100

    Set prog id path

--- src/api/ixaris.js
+++ src/api/ixaris.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 export default {
   login: (values, cb) => {
-    axios.post(window.OPC_API_HOST + '/sessions', values)
+    axios.post(window.OPC_API_HOST + '/98544057818742784/sessions', values)
       .then((res) => cb(null, res.data))
       .catch((e) => cb(e))
   },
   logout: (cb) => {
-    axios.delete(window.OPC_API_HOST + '/sessions')
+    axios.delete(window.OPC_API_HOST + '/98544057818742784/sessions')
       .then((res) => cb(null, res.data))
       .catch((e) => cb(e))
   }
